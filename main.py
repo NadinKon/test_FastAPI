601effe22899e60ec5aecdc3c4b1d984f063cde7
NadinKon <nadinkahf@gmail.com>
2023-06-08 08:34:40 +0300

    *

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://user:*FFGHh658!@localhost:5432/db"
-#DATABASE_URI = 'postgresql://postgres:<password>@localhost/<name_of_the_datbase>'
+DATABASE_URL = "postgresql://postgres:*FFGHh658!@localhost:5432/db"
+# DATABASE_URI = 'postgresql://postgres:<password>@localhost/<name_of_the_datbase>'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -30,20 +30,20 @@ def create_student(student: Student, db: Session = Depends(get_db)):
     db.add(db_student)
     db.commit()
     db.refresh(db_student)
-    return db_student
+    return Student(**db_student.__dict__)
 
 
 @app.get("/students/{student_id}", response_model=Student)
 def read_student(student_id: int, db: Session = Depends(get_db)):
-    db_student = db.query(StudentDB).filter(StudentDB.ID == student_id).first()
+    db_student = db.query(StudentDB).filter(StudentDB.id == student_id).first()
     if db_student is None:
         raise HTTPException(status_code=404, detail="Студент не найден")
-    return db_student
+    return Student(**db_student.__dict__)
 
 
 @app.put("/students/{student_id}", response_model=Student)
 def update_student(student_id: int, student: Student, db: Session = Depends(get_db)):
-    db_student = db.query(StudentDB).filter(StudentDB.ID == student_id).first()
+    db_student = db.query(StudentDB).filter(StudentDB.id == student_id).first()
     if db_student is None:
         raise HTTPException(status_code=404, detail="Студент не найден")
 
@@ -51,12 +51,12 @@ def update_student(student_id: int, student: Student, db: Session = Depends(get_
         setattr(db_student, key, value)
     db.commit()
     db.refresh(db_student)
-    return db_student
+    return Student(**db_student.__dict__)
 
 
 @app.delete("/students/{student_id}")
 def delete_student(student_id: int, db: Session = Depends(get_db)):
-    db_student = db.query(StudentDB).filter(StudentDB.ID == student_id).first()
+    db_student = db.query(StudentDB).filter(StudentDB.id == student_id).first()
     if db_student is None:
         raise HTTPException(status_code=404, detail="Студент не найден")
 
@@ -67,7 +67,8 @@ def delete_student(student_id: int, db: Session = Depends(get_db)):
 
 @app.get("/teachers", response_model=List[Teacher])
 def read_teachers(db: Session = Depends(get_db)):
-    return db.query(TeacherDB).all()
+    db_teachers = db.query(TeacherDB).all()
+    return [Teacher(**teacher.__dict__) for teacher in db_teachers]
 
 
 @app.post("/courses", response_model=Course)
@@ -76,23 +77,23 @@ def create_course(course: Course, db: Session = Depends(get_db)):
     db.add(db_course)
     db.commit()
     db.refresh(db_course)
-    return db_course
+    return Course(**db_course.__dict__)
 
 
 @app.get("/courses/{course_id}", response_model=Course)
 def read_course(course_id: int, db: Session = Depends(get_db)):
-    db_course = db.query(CourseDB).filter(CourseDB.ID == course_id).first()
+    db_course = db.query(CourseDB).filter(CourseDB.id == course_id).first()
     if db_course is None:
         raise HTTPException(status_code=404, detail="Курс не найден")
-    return db_course
+    return Course(**db_course.__dict__)
 
 
 @app.get("/courses/{course_id}/students", response_model=List[Student])
 def read_students_of_course(course_id: int, db: Session = Depends(get_db)):
-    db_students = db.query(StudentDB).filter(GradeDB.КурсID == course_id).all()
+    db_students = db.query(StudentDB).join(GradeDB, GradeDB.Студентid == StudentDB.id).filter(GradeDB.Курсid == course_id).all()
     if not db_students:
         raise HTTPException(status_code=404, detail="Студенты для этого курса не найдены")
-    return db_students
+    return [Student.from_orm(db_student) for db_student in db_students]
 
 
 @app.post("/grades", response_model=Grade)
@@ -101,12 +102,12 @@ def create_grade(grade: Grade, db: Session = Depends(get_db)):
     db.add(db_grade)
     db.commit()
     db.refresh(db_grade)
-    return db_grade
+    return Grade(**db_grade.__dict__)
 
 
 @app.put("/grades/{grade_id}", response_model=Grade)
 def update_grade(grade_id: int, grade: Grade, db: Session = Depends(get_db)):
-    db_grade = db.query(GradeDB).filter(GradeDB.ID == grade_id).first()
+    db_grade = db.query(GradeDB).filter(GradeDB.id == grade_id).first()
     if db_grade is None:
         raise HTTPException(status_code=404, detail="Оценка не найдена")
 
@@ -114,4 +115,4 @@ def update_grade(grade_id: int, grade: Grade, db: Session = Depends(get_db)):
         setattr(db_grade, key, value)
     db.commit()
     db.refresh(db_grade)
-    return db_grade
+    return Grade(**db_grade.__dict__)
--- models.py
+++ models.py
@@ -8,51 +8,68 @@ Base = declarative_base()
 # Модель SQLAlchemy
 class StudentDB(Base):
     __tablename__ = "Студент"
-    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     ФИО = Column(String(200), index=True)
-    ГруппаID = Column(Integer, index=True)
-    УчебныйПланID = Column(Integer, index=True)
+    Группаid = Column(Integer, index=True)
+    УчебныйПланid = Column(Integer, index=True)
+
 
 class TeacherDB(Base):
     __tablename__ = "Преподаватель"
-    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     ФИО = Column(String(200), index=True)
-    ФакультетID = Column(Integer, index=True)
+    Факультетid = Column(Integer, index=True)
+
 
 class CourseDB(Base):
     __tablename__ = "Курс"
-    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     Название = Column(String(200), index=True)
-    Программа_курсаID = Column(Integer, index=True)
+    Программа_курсаid = Column(Integer, index=True)
+
 
 class GradeDB(Base):
     __tablename__ = "Оценка"
-    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    СтудентID = Column(Integer, ForeignKey("Студент.ID"))
-    КурсID = Column(Integer, ForeignKey("Курс.ID"))
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    Студентid = Column(Integer, ForeignKey("Студент.id"))
+    Курсid = Column(Integer, ForeignKey("Курс.id"))
     Оценка = Column(Integer, index=True)
 
 
 # Модель Pydantic
 class Student(BaseModel):
-    ID: int
+    id: int
     ФИО: str
-    ГруппаID: int
-    УчебныйПланID: int
+    Группаid: int
+    УчебныйПланid: int
+
+    class Config:
+        orm_mode = True
+
 
 class Teacher(BaseModel):
-    ID: int
+    id: int
     ФИО: str
-    ФакультетID: int
+    Факультетid: int
+
+    class Config:
+        orm_mode = True
+
 
 class Course(BaseModel):
-    ID: int
+    id: int
     Название: str
-    Программа_курсаID: int
+    Программа_курсаid: int
+
+    class Config:
+        orm_mode = True
+
 
 class Grade(BaseModel):
-    ID: int
-    СтудентID: int
-    КурсID: int
+    id: int
+    Студентid: int
+    Курсid: int
     Оценка: int
 
+    class Config:
+        orm_mode = True
